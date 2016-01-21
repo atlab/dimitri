4b104bb8efacadef13a32fe833f192102ce27bce
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2016-01-21 14:51:07 -0600

    improved caching movies in +pre/getReader.m.

--- +pre/getReader.m
+++ +pre/getReader.m
@@ -1,25 +1,24 @@
 function reader = getReader(key, cache_dir)
 
-
 [path, basename, scanIdx] = fetch1(rf.Session*rf.Scan & key, ...
     'scan_path', 'file_base', 'scan_idx');
 
-if nargin > 1
-    slpos =  strfind(path,'/');
-    stump = path(slpos(end):end);
-    cache_path = [cache_dir stump];
+if nargin <= 1
+    fprintf('Loading data %s\n', path);
+    path = getLocalPath(fullfile(path, sprintf('%s_%03u.tif', basename, scanIdx)));
+    reader = ne7.scanimage.Reader4(path);
+else
+    [~, stump] = fileparts(path);
+    cache_path = fullfile(cache_dir, stump);
     try
         fprintf('Loading data %s\n', cache_path);
         tpath = getLocalPath(fullfile(cache_path, sprintf('%s_%03u.tif', basename, scanIdx)));
         reader = ne7.scanimage.Reader4(tpath);
     catch
-        fprintf('Copying data from %s to %s\n', path, cache_path);
-        copyfile(getLocalPath(path), cache_path);
+        from = fullfile(getLocalPath(path), sprintf('%s_%03u*.tif', basename, scanIdx));
+        fprintf('Copying %s to %s\n', from, cache_path);
+        copyfile(from, cache_path)
         path = getLocalPath(fullfile(cache_path, sprintf('%s_%03u.tif', basename, scanIdx)));
         reader = ne7.scanimage.Reader4(path);
     end
-else
-    fprintf('Loading data %s\n', path);    
-    path = getLocalPath(fullfile(path, sprintf('%s_%03u.tif', basename, scanIdx)));
-    reader = ne7.scanimage.Reader4(path);
 end
