1570953f752402042ae6da318def67e347548776
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2016-01-22 16:18:09 -0600

    migrated schemas +monet and +pre to the cajal repository

--- +monet/getSchema.m
+++
@@ -1,7 +0,0 @@
-function obj = getSchema
-persistent schemaObject
-if isempty(schemaObject)
-    schemaObject = dj.Schema(dj.conn, 'monet', 'dimitri_monet');
-end
-obj = schemaObject;
-end
--- +pre/getSchema.m
+++
@@ -1,7 +0,0 @@
-function obj = getSchema
-persistent schemaObject
-if isempty(schemaObject)
-    schemaObject = dj.Schema(dj.conn, 'pre', 'dimitri_pre');
-end
-obj = schemaObject;
-end
